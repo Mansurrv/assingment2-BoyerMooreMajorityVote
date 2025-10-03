1f11490ac5ad719f8583b3ad433c0078af4aadc9
Mansurrv <buyserikov06@gmail.com>
2025-10-03 18:11:08 +0500

    feat(metrics): performance counters and CSV export

---
+++ src/main/java/org/example/metrics/Metrics.java
@@ -0,0 +1,16 @@
+package org.example.metrics;
+
+public class Metrics {
+    public int comparisons = 0;
+    public int candidateUpdates = 0;
+
+    public void reset() {
+        comparisons = 0;
+        candidateUpdates = 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Comparisons: " + comparisons + ", CandidateUpdates: " + candidateUpdates;
+    }
+}
