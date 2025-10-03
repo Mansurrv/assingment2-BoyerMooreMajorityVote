c987a28f1443a12ac66e7252072bf9f4aed319c4
Mansurrv <buyserikov06@gmail.com>
2025-10-03 19:54:58 +0500

    fix(algorithm, benchmark): added csv file

--- src/main/java/org/example/algorithms/BoyerMooreMajorityVoteAlgorithm.java
+++ src/main/java/org/example/algorithms/BoyerMooreMajorityVoteAlgorithm.java
@@ -25,9 +25,43 @@ public class BoyerMooreMajorityVoteAlgorithm {
 
         count = 0;
         for (int num : nums) {
+            metrics.comparisons++;
             if (num == candidate) count++;
         }
 
         return count > nums.length / 2 ? candidate : -1;
+    }
+
+    public static int findMajorityOptimized(int[] nums, Metrics metrics) {
+        if (nums == null || nums.length == 0) return -1;
+
+        int count = 0;
+        int candidate = -1;
+
+        for (int num : nums) {
+            metrics.comparisons++;
+            if (count == 0) {
+                candidate = num;
+                count = 1;
+                metrics.candidateUpdates++;
+            } else if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        count = 0;
+        int majorityThreshold = nums.length / 2;
+        for (int i = 0; i < nums.length; i++) {
+            metrics.comparisons++;
+            if (nums[i] == candidate) {
+                count++;
+                if (count > majorityThreshold) return candidate;
+            }
+            if (count + (nums.length - i - 1) < majorityThreshold) return -1;
+        }
+
+        return -1;
     }
 }
